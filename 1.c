79cb80bfeb91e3efbf0c2e60b71d34850aed5864
ethan-super <62165783+ethan-super@users.noreply.github.com>
2020-06-02 06:11:57 +0800

    Update 1.c

--- 1.c
+++ 1.c
@@ -3,3 +3,11 @@
 #endif
 ...
 #undef MY_HDR    /* Noncompliant */
+  switch (day) {
+  case MONDAY:
+  case TUESDAY:
+  WEDNESDAY:   // instead of "case WEDNESDAY"
+    doSomething();
+    break;
+  ...
+}
